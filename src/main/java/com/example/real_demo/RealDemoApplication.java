497e3997870bd58eaac22fc4bfe191459322722f
yerkebayev <maratpyn@gmail.com>
2023-03-31 10:38:46 +0900

    Initial commit

---
+++ src/main/java/com/example/real_demo/RealDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.real_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/real_demo/RealDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.real_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
